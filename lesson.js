0530ccb91811220d3fdda6e32b9ebdcf1965c4e8
WM <wmmyers@hotmail.com>
2015-10-19 16:26:56 +0800

    styling css with javascript

--- lesson.js
+++ lesson.js
@@ -1,3 +1,15 @@
-function doAlert(){
+function doAlert() {
   alert("hello!!")
+}
+
+function changeStyle() {
+  var element = document.getElementById('first-container');
+  //toggle colors
+  if (element.style.color === 'white' && element.style.backgroundColor === 'red') {
+    element.style.color = 'red';
+    element.style.backgroundColor = 'white';
+  } else {
+    element.style.color = 'white';
+    element.style.backgroundColor = 'red';
+  }
 }
